5c549d981a5bcfb7716e8e032f66b3b08d2fc2db
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2025-05-10 21:53:14 +0200

    Create arch-chroot-settings.sh

---
+++ arch-chroot-settings.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -n 'LANG=hu_HU.UTF-8
+LC_ADDRESS=hu_HU.UTF-8
+LC_COLLATE=hu_HU.UTF-8
+LC_CTYPE=hu_HU.UTF-8
+LC_IDENTIFICATION=hu_HU.UTF-8
+LC_MEASUREMENT=hu_HU.UTF-8
+LC_MESSAGES=hu_HU.UTF-8
+LC_MONETARY=hu_HU.UTF-8
+LC_NAME=hu_HU.UTF-8
+LC_NUMERIC=hu_HU.UTF-8
+LC_PAPER=hu_HU.UTF-8
+LC_TELEPHONE=hu_HU.UTF-8
+LC_TIME=hu_HU.UTF-8' > /etc/locale.conf
+
+echo -n 'KEYMAP=hu
+FONT=lat2-16
+FONT_MAP=8859-2' > /etc/vconsole.conf
+
+pacman -S networkmanager wpa_supplicant wireless_tools dialog
+systemctl enable NetworkManager.service
