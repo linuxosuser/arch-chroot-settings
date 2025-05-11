d8d9a90b6159259539849537d52fefa67de3332f
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2025-05-11 13:15:03 +0200

    Update systemd-boot-settings.sh

--- systemd-boot-settings.sh
+++ systemd-boot-settings.sh
@@ -8,4 +8,4 @@ initrd  /initramfs-linux.img' > /boot/loader/entries/arch.conf
 echo 'title   Arch Linux Fallback
 linux   /vmlinuz-linux
 initrd  /intel-ucode.img
-initrd  /initramfs-linux-fallback.img > /boot/loader/entries/arch-fallback.conf
+initrd  /initramfs-linux-fallback.img' > /boot/loader/entries/arch-fallback.conf
