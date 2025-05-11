8bd2cca77ec2cfed0f9b59760012a74ad2a31f6d
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2025-05-11 13:13:35 +0200

    Create systemd-boot-settings.sh

---
+++ systemd-boot-settings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /intel-ucode.img
+initrd  /initramfs-linux.img' > /boot/loader/entries/arch.conf
+
+echo 'title   Arch Linux Fallback
+linux   /vmlinuz-linux
+initrd  /intel-ucode.img
+initrd  /initramfs-linux-fallback.img > /boot/loader/entries/arch-fallback.conf
